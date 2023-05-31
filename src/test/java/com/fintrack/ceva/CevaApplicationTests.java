e8e9adc63deb99c35503ab5d95408d3e1072fb03
Julio Cesar Cuevas <jahv20@hotmail.com>
2023-05-30 19:16:53 -0500

    Subiendo App

---
+++ src/test/java/com/fintrack/ceva/CevaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fintrack.ceva;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CevaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
