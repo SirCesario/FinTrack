80805d1fcc64b0b72cd8a35766aca500bad58c4e
Julio Cesar Cuevas <jahv20@hotmail.com>
2023-05-30 20:42:05 -0500

    Cambios en dependencias

---
+++ src/main/java/com/fintrack/ceva/entity/Usuario.java
@@ -0,0 +1,87 @@
+package com.fintrack.ceva.entity;
+
+import jakarta.persistence.*;
+
+
+@Entity
+@Table(name = "usuarios")
+public class Usuario {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nombres;
+    @Column(nullable = false)
+    private String apellidos;
+    @Column(nullable = false, unique = true)
+    private String correo;
+    @Column(nullable = false)
+    private String celular;
+    @Column(nullable = false)
+    private String sexo;
+    //Contructor por defecto para la persistencia
+    public Usuario (){}
+
+    //Contrusctor con parametros
+
+
+    public Usuario( String nombres, String apellidos, String correo, String celular, String sexo) {
+        this.nombres = nombres;
+        this.apellidos = apellidos;
+        this.correo = correo;
+        this.celular = celular;
+        this.sexo = sexo;
+    }
+
+    //Getters y Sett
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombres() {
+        return nombres;
+    }
+
+    public void setNombres(String nombres) {
+        this.nombres = nombres;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        this.apellidos = apellidos;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getCelular() {
+        return celular;
+    }
+
+    public void setCelular(String celular) {
+        this.celular = celular;
+    }
+
+    public String getSexo() {
+        return sexo;
+    }
+
+    public void setSexo(String sexo) {
+        this.sexo = sexo;
+    }
+}
