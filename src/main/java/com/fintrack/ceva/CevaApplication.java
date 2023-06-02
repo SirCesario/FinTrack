9efb68cc3215759eb3f05db42c2e139643038c60
Julio Cesar Cuevas <jahv20@hotmail.com>
2023-06-02 12:57:48 -0500

    Corrigiendo errores

--- src/main/java/com/fintrack/ceva/CevaApplication.java
+++ src/main/java/com/fintrack/ceva/CevaApplication.java
@@ -2,11 +2,10 @@ package com.fintrack.ceva;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 
 @SpringBootApplication
-@EnableSwagger2
 public class CevaApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/fintrack/ceva/repository/UsuarioRepository.java
+++ src/main/java/com/fintrack/ceva/repository/UsuarioRepository.java
@@ -2,9 +2,10 @@ package com.fintrack.ceva.repository;
 
 import com.fintrack.ceva.entity.Usuario;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
-
+@Repository
 public interface UsuarioRepository extends JpaRepository<Usuario, Long> {
     Optional<Usuario> findById(Long id);
     Usuario findByNombres(String nombres);
